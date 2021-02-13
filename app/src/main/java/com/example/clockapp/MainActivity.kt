9365a329897665ed93b2439f45b3b19522bea6eb
shubh <shubhnaik.19@gmail.com>
2021-02-13 12:39:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/clockapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.clockapp
+
+import android.content.Context
+import android.location.Location
+import android.location.LocationManager
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
